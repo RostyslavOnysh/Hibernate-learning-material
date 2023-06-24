4af5b5630ed1d40cc846d15de2f75e617c6dd155
Ros <rospsix@gmail.com>
2023-06-24 14:01:38 +0100

    #10 DAO conception

---
+++ JDBC/src/main/java/org/example/dao/LiteraryFormatDaoImpl.java
@@ -0,0 +1,36 @@
+package org.example.dao;
+
+import org.example.model.LiteraryFormat;
+import org.example.util.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LiteraryFormatDaoImpl implements LiteraryFormatDao {
+    @Override
+    public List<LiteraryFormat> getAll() {
+        List<LiteraryFormat> allFormats = new ArrayList<>();
+        Connection connection = ConnectionUtil.getConnection();
+        Statement getAllFormatsStatment = null;
+        try {
+            getAllFormatsStatment = connection.createStatement();
+            ResultSet resultSet = getAllFormatsStatment
+                    .executeQuery("SELECT * FROM literary_formats");
+            while (resultSet.next()) {
+                String format = resultSet.getString("format");
+                Long id = resultSet.getObject("id", Long.class);
+                LiteraryFormat literaryFormat = new LiteraryFormat();
+                literaryFormat.setId(id);
+                literaryFormat.setFormat(format);
+                allFormats.add(literaryFormat);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Can`t get all formats from DB", e);
+        }
+        return allFormats;
+    }
+}

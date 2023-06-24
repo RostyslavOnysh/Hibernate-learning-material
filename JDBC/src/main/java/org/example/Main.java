f854803329299d2b42eab4b6f922b76163514ac1
Ros <rospsix@gmail.com>
2023-06-24 14:01:54 +0100

    #11 clean main method

--- JDBC/src/main/java/org/example/Main.java
+++ JDBC/src/main/java/org/example/Main.java
@@ -1,34 +1,18 @@
 package org.example;
 
+import org.example.dao.LiteraryFormatDao;
+import org.example.dao.LiteraryFormatDaoImpl;
 import org.example.model.LiteraryFormat;
-import org.example.util.ConnectionUtil;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
+import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) {
-        Connection connection = ConnectionUtil.getConnection();
-        Statement getAllFormatsStatment = null;
-        try {
-            getAllFormatsStatment = connection.createStatement();
-            ResultSet resultSet = getAllFormatsStatment
-                    .executeQuery("SELECT * FROM literary_formats");
-            while (resultSet.next()) {
-                String format = resultSet.getString("format");
-                Long id = resultSet.getObject("id", Long.class);
-                LiteraryFormat literaryFormat = new LiteraryFormat();
-                literaryFormat.setId(id);
-                literaryFormat.setFormat(format);
-                System.out.println(literaryFormat);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException("Can`t get all formats from DB", e);
+        LiteraryFormatDao literaryFormat = new LiteraryFormatDaoImpl();
+        List<LiteraryFormat> allFormats = literaryFormat.getAll();
+        for (LiteraryFormat format : allFormats) {
+            System.out.println(format);
         }
-
     }
 }

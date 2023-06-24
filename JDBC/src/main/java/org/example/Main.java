ca6b9f103ca15761af506a30cb3e96c3f09778ce
Ros <rospsix@gmail.com>
2023-06-24 13:25:58 +0100

    #7 Create object class Properties with username and password

--- JDBC/src/main/java/org/example/Main.java
+++ JDBC/src/main/java/org/example/Main.java
@@ -2,17 +2,21 @@ package org.example;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 
 public class Main {
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            DriverManager.getConnection("jdbc:mysql://localhost:3306/library_db");
+            Properties dbProperties = new Properties();
+            dbProperties.put("user", "root");
+            dbProperties.put("password", "13289812");
+            DriverManager.getConnection("jdbc:mysql://localhost:3306/library_db",dbProperties);
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Can`t load JDBC Driver for MySQL",e);
+            throw new RuntimeException("Can`t load JDBC Driver for MySQL", e);
         } catch (SQLException throwables) {
-            throw new RuntimeException("Can`t create connection to Database",throwables);
+            throw new RuntimeException("Can`t create connection to Database", throwables);
         }
     }
 }

65e41a48932148c62f2e19a3aa383e8170753ca3
Ros <rospsix@gmail.com>
2023-06-25 13:16:07 +0100

    #12 inline Main output

--- JDBC/src/main/java/org/example/Main.java
+++ JDBC/src/main/java/org/example/Main.java
@@ -2,14 +2,10 @@ package org.example;
 
 import org.example.dao.LiteraryFormatDao;
 import org.example.dao.LiteraryFormatDaoImpl;
-import org.example.model.LiteraryFormat;
-
-import java.util.List;
-
 
 public class Main {
     public static void main(String[] args) {
         LiteraryFormatDao literaryFormat = new LiteraryFormatDaoImpl();
-      literaryFormat.getAll().forEach(System.out::println);
+        literaryFormat.getAll().forEach(System.out::println);
     }
 }
